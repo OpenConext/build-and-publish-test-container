151bc3507a0426ae83c097f0e5d96345281c7172
Bart Geesink <bart.geesink@surf.nl>
2023-11-16 20:14:21 +0100

    Add php72 node14 action

---
+++ php72-node14/entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh -l
+DIR_TO_INCLUDE=$1
+USE_YARN=$2
+echo "Run composer install"
+APP_ENV=test composer install --prefer-dist --ignore-platform-reqs --no-interaction
+
+if [ "$USE_YARN" = "true" ]; then
+	echo "Running yarn install and yarn encore install..."
+	yarn install
+	yarn encore production
+else
+	echo "Skipping yarn install and yarn encore install..."
+fi
+
+echo "Create an archive"
+composer archive --file output --no-interaction
+
+echo "Add directories needed for test to the archive"
+tar -rvf output.tar ${DIR_TO_INCLUDE}
