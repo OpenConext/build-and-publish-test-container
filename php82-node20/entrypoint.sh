165899b3b4fbc48d7671813df80043bf698413c8
Bart Geesink <bart.geesink@surf.nl>
2023-11-16 20:32:11 +0100

    php82: Use correct input variables

--- php82-node20/entrypoint.sh
+++ php82-node20/entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/sh -l
-DIR_TO_INCLUDE=$1
-USE_YARN=$2
-echo USE_YARN= $USE_YARN
-echo DIR_TO_INCLUDE = $DIR_TO_INCLUDE
 echo "Run composer install"
-APP_ENV=test composer install --prefer-dist --ignore-platform-reqs --no-interaction
+composer install --prefer-dist --ignore-platform-reqs --no-interaction
 
-if [ "$USE_YARN" = "true" ]; then
+if [ "$INPUT_USE_YARN" = "true" ]; then
 	echo "Running yarn install and yarn encore install..."
 	yarn install
 	yarn encore production
@@ -18,4 +14,4 @@ echo "Create an archive"
 composer archive --file output --no-interaction
 
 echo "Add directories needed for test to the archive"
-tar -rvf output.tar ${DIR_TO_INCLUDE}
+tar -rvf output.tar ${INPUT_DIR_TO_INCLUDE}
