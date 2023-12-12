6c2483f85069fb57a553f758f7cf1d86e164c250
Bart Geesink <bart.geesink@surf.nl>
2023-12-12 11:52:43 +0100

    Tar: Try ustar

--- php82-node20/entrypoint.sh
+++ php82-node20/entrypoint.sh
@@ -16,4 +16,4 @@ echo "Create an archive"
 composer archive --file output --no-interaction
 
 echo "Add directories needed for test to the archive"
-tar --posix -rvf output.tar ${TEST_EXTRA_DIRS}
+tar --format ustar -rvf output.tar ${TEST_EXTRA_DIRS}
